b06581393370fabbff763d7c57e0d9f2a5e3465a
A-2-34-5 <122990706+A-2-34-5@users.noreply.github.com>
2023-02-20 12:04:10 +0700

    Урок 5. Базовые алгоритмы Дз Задача 36

---
+++ Урок 5. Базовые алгоритмы Дз Задача 36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0.
+Console.Clear();
+int[] GetArray(int size, int minimalnoe, int maximalnoe);
+{
+    int[] result = new int[size];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = new Random().Next(minimalnoe, maximalnoe + 1);
+    }
+    return result;
+}
+int Result(int[] inArray)
+{
+    int count = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            count += inArray[i];
+        }
+    }
+    return count;
+}
+int[] array = GetArray(10, -10, 100);
+Console.WriteLine(String.Join(",", array));
+int count = Result(array);
+Console.WriteLine(count);
